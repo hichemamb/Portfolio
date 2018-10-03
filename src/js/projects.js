5929954ce11adeee79187a293203c6998cbb03dc
hichemamb <hichemamb@gmail.com>
2018-10-03 21:49:58 +0200

    dist added

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,13 +1,50 @@
-var item = document.querySelector('.Projects-container__item');
-var overlay = document.querySelector('.Projects-container__item__overlay');
+var input = document.querySelector('.input');
+var maj = document.querySelector('.maj');
+var number = document.querySelector('.number');
+var carspe = document.querySelector('.carspe');
+var carac = document.querySelector('.carac');
+var box = document.querySelector('.box');
 
 
-item.addEventListener('mouseover',function(){
-  overlay.style.display="block";
-  overlay.classList.add('lightSpeedIn');
+var uppercase = /[A-Z]/g;
+var numbers = /[0-9]/g;
+var specialChars = /[!@#$%^&*(),.?":{}|<>]/g;
+
+input.addEventListener('input', function() {
+
+  box.style.display = 'block';
+
+
+  if (input.value.match(uppercase)) {
+    maj.style.color = 'green';
+  } else {
+    maj.style.color = 'red';
+  }
+
+  if (input.value.match(numbers)) {
+    number.style.color = 'green';
+  } else {
+    number.style.color = 'red';
+  }
+
+  if (input.value.length >= 8) {
+    carac.style.color = 'green';
+  } else {
+    carac.style.color = 'red';
+  }
+
+  if (input.value.match(specialChars)) {
+    carspe.style.color = 'green';
+  } else {
+    carspe.style.color = 'red';
+  }
+
 })
 
-item.addEventListener('mouseout',function(){
-  overlay.style.display="none";
-  overlay.classList.remove('lightSpeedIn');
+input.addEventListener('blur', function() {
+  box.style.display = 'none';
+})
+
+input.addEventListener('focus', function() {
+  box.style.display = 'block';
 })
